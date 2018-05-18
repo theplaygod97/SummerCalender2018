1f6d2aa175a82adff3b9d1198e35c057194d7ef7
theplaygod97 <sasgt97@hmail.com>
2018-05-18 14:16:56 +0530

    Added Alarm

--- app/src/main/java/com/example/satya/summercalender2018/MainActivity.java
+++ app/src/main/java/com/example/satya/summercalender2018/MainActivity.java
@@ -1,35 +1,68 @@
 package com.example.satya.summercalender2018;
 
 
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.graphics.Color;
-
-import android.os.Build;
-
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.widget.DrawerLayout;
-
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.CalendarView;
-
-import android.widget.Toast;
-
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.transition.Explode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.ImageView;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String uniqueID ="com.example.satya.summercalender2018" ;
-@Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+        setupWindowAnimations();
+
+        int X=Calendar.DAY_OF_MONTH;
+        final ArrayList<Integer> alarmDays= new ArrayList<>();
+        alarmDays.add(X);
+
+
+        Button button=(Button)findViewById(R.id.onClickAlarm);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void  onClick(View v) {
+                SSS(alarmDays);
+            }
+            void SSS(ArrayList<Integer> A){
+                Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
+                intent.putExtra(AlarmClock.EXTRA_HOUR,12);
+                intent.putExtra(AlarmClock.EXTRA_MINUTES,00);
+                intent.putExtra(AlarmClock.EXTRA_MESSAGE,"PPT CLASS 3:00 PM");
+                intent.putExtra(AlarmClock.EXTRA_DAYS,A);
+                startActivity(intent);
+            }
+        });
+
+
         android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("v4.0");
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
             drawer,
@@ -38,7 +71,8 @@ public class MainActivity extends AppCompatActivity {
             R.string.navigation_drawer_close);
       drawer.addDrawerListener(toggle);
       toggle.syncState();
-      CalendarView calendarView;
+      final CalendarView calendarView;
+
 
         calendarView = (CalendarView) findViewById(R.id.simpleCalendarView); // get the reference of CalendarView
         calendarView.setFocusedMonthDateColor(Color.RED); // set the red color for the dates of  focused month
@@ -46,12 +80,11 @@ public class MainActivity extends AppCompatActivity {
         calendarView.setSelectedWeekBackgroundColor(Color.RED); // red color for the selected week's background
         calendarView.setWeekSeparatorLineColor(Color.GREEN); // green color for the week separator line
 
-
         // perform setOnDateChangeListener event on CalendarView
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
 
-             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
 
                 String S1="Higher Maths - E 114";
                 String S2="Arithmetic - E 314";
@@ -60,6 +93,122 @@ public class MainActivity extends AppCompatActivity {
                 String S5="English 2 - E 515";
                 String S6="INTERNSHIP";
                 String S7="NO CLASSES";
+
+                int D=dayOfMonth;int X=0;
+                //MAY
+                if(month==4&&(D==5||D==12||D==19||D==26))
+                    X=0;
+                else if(month==4&&(D==6||D==13||D==20||D==27))
+                    X=1;
+                else if(month==4&&(D==7||D==14||D==21||D==28))
+                    X=2;
+                else if(month==4&&(D==1||D==8||D==15||D==22||D==29))
+                    X=3;
+                else if(month==4&&(D==2||D==9||D==16||D==23||D==30))
+                    X=4;
+                else if(month==4&&(D==3||D==10||D==17||D==24||D==31))
+                    X=5;
+                else if(month==4&&(D==4||D==11||D==18||D==25))
+                    X=5;
+                //JUNE
+                else if(month==5&&(D==2||D==9||D==16||D==23||D==30))
+                    X=0;
+                else if(month==5&&(D==3||D==10||D==17||D==24))
+                    X=1;
+                else if(month==5&&(D==4||D==11||D==18||D==25))
+                    X=2;
+                else if(month==5&&(D==5||D==12||D==19||D==26))
+                    X=3;
+                else if(month==5&&(D==6||D==13||D==20||D==27))
+                    X=4;
+                else if(month==5&&(D==7||D==14||D==21||D==28))
+                    X=5;
+                else if(month==5&&(D==1||D==8||D==15||D==22||D==29))
+                    X=5;
+
+
+                final ArrayList<Integer> alarmDays= new ArrayList<>();
+                alarmDays.add(X);
+
+
+
+
+                Button button=(Button)findViewById(R.id.onClickAlarm);
+                button.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void  onClick(View v) {
+                        Calendar cal = Calendar.getInstance();
+                        //24 hour format
+                        int hourofday = cal.get(Calendar.HOUR_OF_DAY);
+                        if (hourofday >= 13 && hourofday <= 18) {        // Creating alert Dialog with two Buttons
+
+                            AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+
+                            // Setting Dialog Title
+                            alertDialog.setTitle("Three o’clock is always too late or too early for anything you want to do.");
+
+                            // Setting Dialog Message
+                            alertDialog.setMessage("Either u missed today's class or you are too early for the next one.");
+
+                            // Setting Icon to Dialog
+                            alertDialog.setIcon(R.drawable.ic_copyright_black_24dp);
+
+                            // Setting Positive "Yes" Button
+                            alertDialog.setPositiveButton("HMMMM",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int which) {
+
+                                            Toast T=new Toast(getApplicationContext());
+                                            ImageView view = new ImageView(getApplicationContext());
+                                            view.setImageResource(R.drawable.ic_code_black_24dp);
+                                            T.setView(view);
+                                            T.show();
+
+                                            // Write your code here to execute after dialog
+                                            Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
+                                            intent.putExtra(AlarmClock.EXTRA_HOUR, 12);
+                                            intent.putExtra(AlarmClock.EXTRA_MINUTES, 00);
+                                            intent.putExtra(AlarmClock.EXTRA_MESSAGE, "CLASS");
+                                            intent.putExtra(AlarmClock.EXTRA_DAYS, alarmDays);
+                                            startActivity(intent);
+
+                                            dialog.cancel();
+                                        }
+                                    });
+
+                            // Setting Negative "NO" Button
+                            alertDialog.setNegativeButton("OK",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            // Write your code here to execute after dialog
+                                            Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
+                                            intent.putExtra(AlarmClock.EXTRA_HOUR, 12);
+                                            intent.putExtra(AlarmClock.EXTRA_MINUTES, 00);
+                                            intent.putExtra(AlarmClock.EXTRA_MESSAGE, "CLASS");
+                                            intent.putExtra(AlarmClock.EXTRA_DAYS, alarmDays);
+                                            startActivity(intent);
+                                            Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT).show();
+                                            dialog.cancel();
+                                        }
+                                    });
+
+                            // Showing Alert Message
+                            alertDialog.show();
+                        } else {
+                            Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
+                            intent.putExtra(AlarmClock.EXTRA_HOUR, 12);
+                            intent.putExtra(AlarmClock.EXTRA_MINUTES, 00);
+                            intent.putExtra(AlarmClock.EXTRA_MESSAGE, "CLASS");
+                            intent.putExtra(AlarmClock.EXTRA_DAYS, alarmDays);
+                            startActivity(intent);
+                        }
+                    }
+
+                });
+
+
+
                 // display the selected date by using a toast
 
                 if(year==2018 && month==4 && dayOfMonth ==4) {
@@ -168,7 +317,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 else{
                     Cal1notify(dayOfMonth,month,year,S7);
-                    Toast.makeText(getApplicationContext(), dayOfMonth + "/" + (month+1) + "/" + year + " " + "NO CLASSES", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), D + "/" + (month+1) + "/" + year + " " + "NO CLASSES", Toast.LENGTH_LONG).show();
                 }
             }
 
@@ -247,6 +396,12 @@ public class MainActivity extends AppCompatActivity {
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
         }
+    }
+
+    private void setupWindowAnimations() {
+        Explode fade = new Explode();
+        fade.setDuration(1000);
+        getWindow().setEnterTransition(fade);
     }
 
 
